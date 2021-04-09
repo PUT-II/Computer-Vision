89c432b28806d39378d35a87e47b7e6654d84a6e
Gunock <asz.czyk@gmail.com>
2021-04-09 21:54:14 +0200

    Added rectangle fragment size normalization

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
+import math
 import os
 from typing import List, Dict
 
 import cv2 as cv
-import imutils
 import numpy as np
 
 
@@ -11,47 +11,78 @@ def calculate_length(point1, point2):
 
 
 def preprocess_image(img: np.ndarray) -> np.ndarray:
+    # Copy image and padding
     res_img = img.copy()
+    res_img = cv.copyMakeBorder(res_img, 64, 64, 64, 64, cv.BORDER_CONSTANT)
+
+    # Get shape contours
     _, res_img = cv.threshold(res_img, 127, 255, cv.THRESH_BINARY)
     contours, _ = cv.findContours(res_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-    # gray = cv.GaussianBlur(res_img, (3, 3), 0)
-    # mask = np.zeros(gray.shape, dtype="uint8")
-    # cv.drawContours(mask, [contours[0]], -1, 255, -1)
+    # Approximate contours to polygon
+    epsilon = 0.005 * cv.arcLength(contours[0], closed=True)
+    approx = cv.approxPolyDP(contours[0], epsilon, closed=True)
 
-    x, y, w, h = cv.boundingRect(contours[0])
+    # Find longest line and straighten image
+    start_point, end_point = get_longest_line_points(approx)
+    angle = math.atan2((end_point[1] - start_point[1]), (end_point[0] - start_point[0])) * (180.0 / math.pi)
+    angle %= 180.0
+
+    moments = cv.moments(contours[0])
+    center_x = int(moments["m10"] / moments["m00"])
+    center_y = int(moments["m01"] / moments["m00"])
+    rot_mat = cv.getRotationMatrix2D((center_y, center_x), angle, 1.0)
+
+    height, width = res_img.shape
+    res_img = cv.warpAffine(res_img, rot_mat, (width, height), flags=cv.INTER_LINEAR)
+
+    # Cut black padding from image
+    contours, _ = cv.findContours(res_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours = contours[0]
+
+    x, y, w, h = cv.boundingRect(contours)
     res_img = res_img[y:y + h, x:x + w]
-    # maskROI = mask[y:y + h, x:x + w]
-    # imageROI = cv.bitwise_and(res_img, res_img, mask=maskROI)
-    # rot_img = imutils.rotate_bound(imageROI, 50)
-    rot_img = res_img
-    
-    img_h, img_w = rot_img.shape
-    scale_x = img_w / 496
-    scale_y = img_h / 496
+
+    # Scale image so that at least one dimension is 512px and other lesser that 512px
+    img_h, img_w = res_img.shape
+    scale_x = img_w / 512
+    scale_y = img_h / 512
 
     scale = scale_y if scale_x < scale_y else scale_x
 
     new_width = round(img_w / scale)
     new_height = round(img_h / scale)
-    rot_img = cv.resize(rot_img, (new_width, new_height))
-    _, rot_img = cv.threshold(rot_img, 127, 255, cv.THRESH_BINARY)
+    res_img = cv.resize(res_img, (new_width, new_height))
 
-    pad_x = round((512 - new_width) / 2)
-    pad_y = round((512 - new_height) / 2)
-    rot_img = cv.copyMakeBorder(rot_img, pad_y, pad_y, pad_x, pad_x, cv.BORDER_CONSTANT)
-    rot_img = cv.resize(rot_img, (512, 512))
+    # Threshold image after resize (removes intermediate colors)
+    _, res_img = cv.threshold(res_img, 127, 255, cv.THRESH_BINARY)
 
-    return rot_img
+    return res_img
 
 
-def calculate_longest_dege(contour):
-    longest_edge = 0.0
+def get_longest_line_points(contour):
+    """ Finds longest line in contour
+
+    :param contour:
+    :return: Tuple with start and end point of line
+    """
+    result = (0, 0), (0, 0)
+    result_length = 0.0
+
     for j in range(len(contour) - 1):
         length = calculate_length(contour[j][0], contour[j + 1][0])
-        if length > longest_edge:
-            longest_edge = length
-    return longest_edge
+        if length > result_length:
+            result = contour[j][0], contour[j + 1][0]
+            result_length = length
+
+    return tuple(result[0]), tuple(result[1])
+
+
+def approximate_contours(contours):
+    epsilon = 0.005 * cv.arcLength(contours[0], closed=True)
+    approx = cv.approxPolyDP(contours[0], epsilon, closed=True)
+    return approx
+
 
 class Dataset:
     SHOW_IMAGES_ON_LOAD = False
@@ -64,51 +95,59 @@ class Dataset:
         self.images = []
         self.edge_lengths: Dict[int, float] = {}
 
-    def approx_fit(self, contours, i):
-        epsilon = 0.005 * cv.arcLength(contours[0], closed=True)
-        approx = cv.approxPolyDP(contours[0], epsilon, closed=True)
-        return approx
-
     def solve(self):
         print()
-        approx_points: Dict[int, list] = {}
-        area_points: Dict[int, list] = {}
+        approx_vertices: Dict[int, list] = {}
+        images_area: Dict[int, np.int32] = {}
 
         for i, image in enumerate(self.images):
             contours, hierarchy = cv.findContours(image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-            approx = self.approx_fit(contours, i)
-            area = cv.contourArea(approx)
+            approx = approximate_contours(contours)
 
-            if len(approx) not in approx_points.keys():
-                approx_points[len(approx)] = [i]
+            # TODO: Decide which area is better
+            # area = cv.contourArea(approx)
+            area = np.sum(image, dtype=np.int32)
+            images_area[i] = area
+
+            if len(approx) not in approx_vertices.keys():
+                approx_vertices[len(approx)] = [i]
             else:
-                approx_points[len(approx)] += [i]
+                approx_vertices[len(approx)] += [i]
 
-            area_points[i] = area
-
-            img_copy = cv.cvtColor(image.copy(), cv.COLOR_GRAY2BGR)
-            cv.drawContours(img_copy, [approx], 0, color=(0, 0, 255), thickness=2)
             if Dataset.SHOW_IMAGES:
+                img_copy = cv.cvtColor(image.copy(), cv.COLOR_GRAY2BGR)
+                cv.drawContours(img_copy, [approx], 0, color=(0, 0, 255), thickness=2)
                 cv.imshow("test", img_copy)
                 cv.waitKey()
-        print(f"Vertices: {approx_points}")
-        print(f"Area_points: {area_points}")
+
+        result = {}
+        expected_area = np.int32(448 * 448 * 255)
+        for i in range(len(self.images) - 1):
+            for j in range(i + 1, len(self.images)):
+                joint_area = images_area[i] + images_area[j]
+                deviance = np.abs(expected_area - joint_area)
+                if deviance < 100000:
+                    print(f"{i}-{j} : {deviance}")
+
+        # TODO: Find best way to match rectangle fragments
+        print(f"Vertices: {approx_vertices}")
+        print(f"Area: {images_area}")
         # print()
         # print(self.edge_lengths)
         # print()
         # print(images_points)
-        result = {}
-        remaining = []
-        for elem in approx_points.values():
-            if len(elem) == 2:
-                result[elem[0]] = elem[1]
-                result[elem[1]] = elem[0]
-            elif len(elem) == 1:
-                remaining.append(elem[0])
-
-        if len(remaining) == 2:
-            result[remaining[0]] = remaining[1]
-            result[remaining[1]] = remaining[0]
+        # result = {}
+        # remaining = []
+        # for elem in approx_points.values():
+        #     if len(elem) == 2:
+        #         result[elem[0]] = elem[1]
+        #         result[elem[1]] = elem[0]
+        #     elif len(elem) == 1:
+        #         remaining.append(elem[0])
+        #
+        # if len(remaining) == 2:
+        #     result[remaining[0]] = remaining[1]
+        #     result[remaining[1]] = remaining[0]
 
         print(f"Correct : {self.correct}")
         print(f"Result : {result}")
